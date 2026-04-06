9b84a5cbb2de2ddffc9a5acbda0013156cab81b3
Satya Prasad A <satya.a@diplsrv.local>
2026-04-06 13:06:33 +0530

    updated both chatcontroller and index'

--- src/main/java/com/example/demo/ChatController.java
+++ src/main/java/com/example/demo/ChatController.java
@@ -7,19 +7,24 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class ChatController 
-{
+public class ChatController {
 
-	 @Autowired
-	 private SimpMessagingTemplate messagingTemplate;
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
 
-	 @MessageMapping("/send")
-	 @SendTo("/topic/messages")
-	 public ChatMessage send(ChatMessage message) 
-	 {
+    @MessageMapping("/send")
+    @SendTo("/topic/messages")
+    public ChatMessage send(ChatMessage message) {
 
-	        System.out.println("Client sent: " + message.getSender() + " -> " + message.getContent());
+        System.out.println("Client sent: " + message.getSender() + " -> " + message.getContent());
 
-	        return message;
-	    }
+        return message;
+    }
+
+    @MessageMapping("/typing")
+    @SendTo("/topic/typing")
+    public String typing(String sender) {
+
+        return sender;
+    }
 }
